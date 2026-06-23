01243c8e7ae11e428f7511d80c78c984e4359f33
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-06-23 22:48:12 +0530

    Ride service Kafka config completed

--- ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
@@ -1,4 +1,31 @@
 package com.rideshare.rideservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
 public class KafkaConfig {
+
+    //Topic where Ride Service published ride Request
+    //Matching service Subscribers to this topic
+
+    @Bean
+    public NewTopic rideRequestedTopic(){
+        return TopicBuilder.name("ride.requested")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    //Topic where Matching Service publishes match results
+    //Ride Service subscribers to this topic
+
+    @Bean
+    public NewTopic rideMatchedTopic(){
+        return TopicBuilder.name("ride.matched")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
 }
