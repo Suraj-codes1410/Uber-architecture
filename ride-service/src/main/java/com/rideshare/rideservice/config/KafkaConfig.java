9e0f3f21748ec3d2eac68ba92269dabc63f926f9
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-06-28 22:26:03 +0530

    kafka update for rideService

--- ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
@@ -1,9 +1,22 @@
 package com.rideshare.rideservice.config;
 
+import com.rideshare.rideservice.event.RideRequestedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConfig {
 
     //Topic where Ride Service published ride Request
@@ -28,4 +41,32 @@ public class KafkaConfig {
                 .build();
     }
 
+
+    @Bean
+    public ProducerFactory<String, RideRequestedEvent> producerFactory() {
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+
+        props.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+
+        props.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class
+        );
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, RideRequestedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
